422d91822dc416c2dcb758daf0846085bb2ff4c5
Nishant Buwa <nishantbuwa01@gmail.com>
2025-06-21 23:35:35 +0530

    login page input size fixed

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -52,7 +52,7 @@ function Login({setIsAuthenticated, setUserInfo}) {
                             name='email'
                             onChange={handleChange}
                             value={formData.email}
-                            className='flex-1 px-2 py-1 text-black outline-none'
+                            className='flex-1 px-2 py-1 text-black outline-none sm:w-auto w-[80px]'
                             required />
                     </div>
                     <div className="flex items-center gap-2">
@@ -61,7 +61,7 @@ function Login({setIsAuthenticated, setUserInfo}) {
                             name='password'
                             onChange={handleChange}
                             value={formData.password}
-                            className='flex-1 px-2 py-1 text-black outline-none'
+                            className='flex-1 px-2 py-1 text-black outline-none sm:w-auto w-[80px]'
 
                             required />
                     </div>
